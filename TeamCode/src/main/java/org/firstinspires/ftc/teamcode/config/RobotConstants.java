90f153320c18e390870c21e9809edaa6ab480e34
itzSamar <samarreal15@gmail.com>
2026-08-10 15:49:44 -0700

    PP updates plus folder integration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/RobotConstants.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.config;
 
 public class RobotConstants {
+
+    public final static double robotMass = (double) 9.757;
+
+    //Swerve generic
+    public final static double robotLength = (double) 356.7; // in mm
+    public final static double maxDrivePower = (double) 1.0;
+
+    //PIDF
+    public final static double Proportional = 0;
+    public final static double Integral = 0;
+    public final static double Derivative = 0;
+    public final static double Feedforward = 0;
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,15 +1,25 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
+import static org.firstinspires.ftc.teamcode.config.RobotConstants.*;
+
+
+//basic constants rn based on 23511
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.SwerveConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
-
+    public static FollowerConstants followerConstants = new FollowerConstants()
+        .mass(robotMass);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static SwerveConstants swerveConstants = new SwerveConstants()
+            .maxPower(maxDrivePower); //determines the max power of the drivetrain
+    //      .zeroPowerBehavior(SwerveConstants.ZeroPowerBehavior.IGNORE_ANGLE_CHANGES)
+    //the above disables x locking for swerve, which can be useful for tuning pod offsets
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
